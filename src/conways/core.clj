56416973509fa97906d4b3d6ac4c312c4583fb21
Adrian Mowat <adrian.mowat@gmail.com>
2011-12-04 21:53:09 +0000

    First working version

--- src/conways/core.clj
+++ src/conways/core.clj
@@ -7,7 +7,7 @@
 	 (- below 1) below (+ below 1)]))
 
 (defn values-at [posns source]
-	(map #(get source %1) posns))
+	(map #(get source %1 0) posns))
 
 (defn count-live-neighbours [pos grid x-size]
 	(reduce + (values-at (immedaiate-neighbours pos x-size) grid)))
@@ -17,3 +17,43 @@
 		(let [will-live {2 1 3 1}]
 			(get will-live live-neighbours 0))
 		(if (= 3 live-neighbours) 1 0)))
+
+(def zero-indexed-seq (iterate inc 0))
+(def one-indexed-seq (iterate inc 1))
+
+(defn next-generation [grid x-size] 
+	(into [] (map 
+		(fn [pos curr-state] 
+			(next-value (count-live-neighbours pos grid x-size) curr-state))
+		zero-indexed-seq grid)))
+
+(defn newlines [x-size] 
+	(map #(if (= 0 (mod %1 x-size)) "\n" "") one-indexed-seq))		
+
+(defn translate-grid [grid x-size] 
+	(map #(str (if (= 1 %1) "X" " ") %2) grid (newlines x-size)))		
+
+(defn print-grid [grid x-size]
+	(reduce str "" (translate-grid grid x-size)))
+
+(def start
+	[0 0 0 0 0 0 0 0
+	 0 0 1 1 1 0 0 0
+	 0 1 1 1 0 0 0 0
+	 0 0 0 0 0 0 0 0
+	 0 0 0 0 0 1 0 0
+	 0 0 0 0 0 1 0 0
+	 0 0 0 0 0 1 0 0
+	 0 0 0 0 0 0 0 0
+	])	
+
+
+(defn live-cells [grid] (reduce + grid))
+
+(defn do-game [grid x-size]
+	(println (print-grid grid x-size))
+	(Thread/sleep 250)
+	(let [next-gen (next-generation grid x-size)]
+		(if (not (zero? (live-cells next-gen))) 
+			(recur next-gen x-size))))
+
--- test/conways/test/core.clj
+++ test/conways/test/core.clj
@@ -2,6 +2,7 @@
   (:use [conways.core])
   (:use [clojure.test]))
 
+; Finding immediate neighbours
 (deftest test-immediate-neighbours-for-pos-6 ;; FIXME: write
   (is (= [1 2 3 5 7 9 10 11] (immedaiate-neighbours 6 4)) "neighbours for pos 6, x size 4"))
 
@@ -14,12 +15,17 @@
 (deftest test-values-at-2
 	(is (= [1 0 1] (values-at [1 2 3] [0 1 0 1 0]))))
 
+(deftest test-values-at-bad-index-is-zero
+	(is (= [1 0 0] (values-at [1 2 -1] [0 1 0 1 0]))))
+
+
 (deftest test-count-live-neighbours
 	(is (= 0 (count-live-neighbours 6 [0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0] 4))))	
 
 (deftest test-count-live-neighbours-2
 	(is (= 1 (count-live-neighbours 6 [0 0 0 0 0 1 1 0 0 0 0 0 0 0 0 0] 4))))	
 
+; Applying Conway's rules to a cell
 (deftest test-next-value-live-cell-with-1-live-neighbour
 	(is (= 0 (next-value 1 1)) 
 		"live cell with 1 live neighbour dies"))
@@ -47,3 +53,21 @@
 (deftest test-next-value-dead-cell-with-4-live-neighbours
 	(is (= 0 (next-value 4 0)) 
 		"dead cell with 4 live neighbours stays dead"))	
+
+; Running the game
+(deftest test-applying-rules-to-grid
+	(is (= [0 0 0 0] (next-generation [0 1 0 0] 2))))
+
+(deftest test-newlines
+	(is (= ["" "\n"] (take 2 (newlines 2)))))	
+
+(deftest test-translate-grid
+	(is (= [" " "X\n"] (translate-grid [0 1] 2))))
+
+(def grid "  X \n    \n")
+
+(deftest test-printing-grid
+	(is (= grid (print-grid [0 0 1 0 0 0 0 0] 4))))	
+
+
+
